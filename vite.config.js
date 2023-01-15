21dd314be6c50e968023e7a06b9e42a5e67ad72c
rxb3rth <reliutg@gmail.com>
2023-01-15 04:11:17 +0100

    fix: build __vite-browser-external

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  optimizeDeps: {
+    exclude: [
+      "@iconify/utils/lib/loader/fs",
+      "@iconify/utils/lib/loader/install-pkg",
+      "@iconify/utils/lib/loader/node-loader",
+      "@iconify/utils/lib/loader/node-loaders",
+    ],
+  },
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      external: [
+        "@iconify/utils/lib/loader/fs",
+        "@iconify/utils/lib/loader/install-pkg",
+        "@iconify/utils/lib/loader/node-loader",
+        "@iconify/utils/lib/loader/node-loaders",
+      ],
+      input: ["./index.html"],
+    },
+  },
+});
